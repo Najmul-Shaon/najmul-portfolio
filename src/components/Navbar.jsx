367e89ef3a09ba0f562b3899d046222510e1d314
Najmul shaon <najmul.nh.shaon@gmail.com>
2025-05-03 16:41:34 +0600

    remove duplicate project on the menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -74,16 +74,7 @@ const Navbar = () => {
               </div>
             </a>
           </li>
-          <li>
-            <a
-              className="block px-4 py-2 no-underline outline-none hover:no-underline"
-              href="/#projects"
-            >
-              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600 uppercase">
-                PROJECTS
-              </div>
-            </a>
-          </li>
+
           <li>
             <a
               className="block px-4 py-2 no-underline outline-none hover:no-underline"
