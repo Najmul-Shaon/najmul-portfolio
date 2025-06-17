bc702dbd7cd888ffc9801940c36bab06ec91b107
Najmul shaon <najmul.nh.shaon@gmail.com>
2025-06-17 23:56:44 +0600

    experience added

--- src/assets/data/experience.js
+++ src/assets/data/experience.js
@@ -1,12 +1,18 @@
 export const experiences = [
   {
     id: 1,
+    title: "Front-end Developer",
+    company: "Softvence Agency.",
+    duration: "(May 2025 - Present)",
+  },
+  {
+    id: 2,
     title: "Assistant Engineer",
     company: "OnnoRokom Software Limited.",
     duration: "(May 2022 - Dec 2024)",
   },
   {
-    id: 2,
+    id: 3,
     title: "IT Operator",
     company: "Beacon Pharmaceuticals Limited.",
     duration: "(Dec 2020 - May 2022)",
